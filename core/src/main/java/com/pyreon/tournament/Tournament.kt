3744bc131493e0b506e5e58647d098d51ae1cc61
Ray Guessford <rguessford@gmail.com>
2019-10-25 15:00:04 -0500

    UI stuff works with its own viewport

--- core/src/main/java/com/pyreon/tournament/Tournament.kt
+++ core/src/main/java/com/pyreon/tournament/Tournament.kt
@@ -4,15 +4,22 @@ import com.badlogic.gdx.Game
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.g2d.TextureAtlas
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
 
 class Tournament : Game() {
     lateinit var batch: SpriteBatch
     lateinit var atlas: TextureAtlas
+    lateinit var uiAtlas: TextureAtlas
+    lateinit var uiSkin:Skin
 
 
     override fun create() {
         batch = SpriteBatch()
         atlas = TextureAtlas(Gdx.files.internal("packed/assets.atlas"))
+        uiAtlas = TextureAtlas(Gdx.files.internal("packed/uiskin.atlas"))
+        uiSkin = Skin(Gdx.files.internal("packed/uiskin.json"))
+        uiSkin.addRegions(uiAtlas)
+
         this.setScreen(TestScreen(this))
     }
 
